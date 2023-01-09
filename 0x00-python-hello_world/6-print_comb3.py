39e2cff4645d3529801cb12d908add5710dc08f3
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-09 16:27:26 -0600

    Still trying to fix PEP8

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -2,8 +2,7 @@
 
 unique_combinations = [
     list(z) for z in [
-        set(y) for y in
-            [
+        set(y) for y in [
                 set(x) for x in ["{:02d}".format(x) for x in range(1, 100)]
             ]
         ] if len(z) == 2
@@ -12,8 +11,7 @@ for combination in unique_combinations:
     combination.sort()
 
 unique_combinations = [
-    int(y) for y in
-        set(
+    int(y) for y in set(
             ["".join(x) for x in unique_combinations]
         )
     ]
