193bb7b408a90597bd60035592378c3b3f67b70d
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-09 16:59:06 -0600

    first attempt at the upper task

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+
+def uppercase(str):
+
+    value_of_a = 97
+    length_of_alphabet = 26
+    difference = 32
+
+    characters = [x for x in str]
+
+    message = "".join(
+        [
+        y if ord(y) < value_of_a or ord(y) > value_of_a + length_of_alphabet
+        else 
+        chr(ord(y) - difference) for y in characters
+        ]
+    )
+    print(message)
