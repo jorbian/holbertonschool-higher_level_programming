491d6f16938a3bc918d3ed466c2f785e36a2e3ea
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-09 13:43:32 -0600

    Fixed PEP8 violations?

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def fizzbuzz(
     factors=[
         {
@@ -9,7 +10,7 @@ def fizzbuzz(
         {
             "word": "Buzz",
             "num": 5
-        }
+        },
         ]
 ):
     for x in range(1,101):
