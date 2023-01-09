bd76f4c157393b3000e71d65eb9dde80e65943b1
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-09 16:26:26 -0600

    Trying again.

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 unique_combinations = [
-    list(z) for z in 
-        [
-        set(y) for y in 
+    list(z) for z in [
+        set(y) for y in
             [
                 set(x) for x in ["{:02d}".format(x) for x in range(1, 100)]
             ]
@@ -12,8 +11,12 @@ unique_combinations = [
 for combination in unique_combinations:
     combination.sort()
 
-unique_combinations = [int(y) for y in set(["".join(x) for x in unique_combinations])]
-
+unique_combinations = [
+    int(y) for y in
+        set(
+            ["".join(x) for x in unique_combinations]
+        )
+    ]
 unique_combinations.sort()
 
 unique_combinations = ", ".join(
