a9d9a56acd7d5a3f817c7bd571adf63bc5d9fc1a
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-09 13:37:22 -0600

    Updated file structure and completed fizzbuzz.

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+def fizzbuzz(
+    factors=[
+        {
+            "word": "Fizz",
+            "num": 3
+        },
+        {
+            "word": "Buzz",
+            "num": 5
+        }
+        ]
+):
+    for x in range(1,101):
+
+        say = ""
+
+        for factor in factors:
+            if (x % factor["num"] == 0):
+                say += factor["word"]
+
+        if (len(say) == 0):
+            say = str(x)
+
+        print("{} ".format(say), end="")
