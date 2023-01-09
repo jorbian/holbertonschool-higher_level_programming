4208b9e0778e3a058dced5366d860c5816c97569
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-09 16:24:14 -0600

    Weird convoluted solution that I just hope works.

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+
+unique_combinations = [
+    list(z) for z in 
+        [
+        set(y) for y in 
+            [
+                set(x) for x in ["{:02d}".format(x) for x in range(1, 100)]
+            ]
+        ] if len(z) == 2
+]
+for combination in unique_combinations:
+    combination.sort()
+
+unique_combinations = [int(y) for y in set(["".join(x) for x in unique_combinations])]
+
+unique_combinations.sort()
+
+unique_combinations = ", ".join(
+    "{:02d}".format(x) for x in
+    unique_combinations
+)
+print(unique_combinations)
