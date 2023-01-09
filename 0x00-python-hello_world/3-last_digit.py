1785472ccf9eb95141e770b6c428c8e50cd5f275
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-09 15:03:20 -0600

    Attempt at a functional solution

---
+++ 0x00-python-hello_world/3-last_digit.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import random
+
+number = random.randint(-10000, 10000)
+
+last_digit = abs(number) % 10
+
+status_determiner = {
+    (lambda x: x == 0): "and is 0",
+    (lambda x: x > 5): "and is greater than 5",
+    (lambda x: x < 6): "and is less than 6 and not 0"
+}
+
+for key, value in status_determiner.items():
+    if (key(last_digit)):
+        status = value
+
+message = "Last digit of {} is {} {}".format(
+    str(number),
+    str(last_digit),
+    status
+)
+print(message)
