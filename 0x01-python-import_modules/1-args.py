d52964b46987dd2d5fdeccb71638bc6c5839f937
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-10 20:08:14 -0600

    First attempt at args task

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,2 +1,29 @@
 #!/usr/bin/python3
 
+
+if __name__ == "__main__":
+
+    import sys
+
+    num_of_args = len(sys.argv) - 1
+
+    status = [
+        None,
+        "argument",
+        "arguments"
+    ][
+        [
+        num_of_args == 0,
+        num_of_args == 1,
+        num_of_args >= 2
+        ].index(True)
+    ]
+
+    if status:
+
+        print("{} {}".format(num_of_args, status))
+
+        for i in range(num_of_args):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    else:
+        print("0 arguments.")
