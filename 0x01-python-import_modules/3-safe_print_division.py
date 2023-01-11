79f8defc2c99d4554b99fcca34041ee64d965e39
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-10 22:01:15 -0600

    asdf

--- 0x01-python-import_modules/3-safe_print_division.py
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -2,4 +2,11 @@
 
 
 def safe_print_division(a, b):
-    pass
+    result = "None"
+    try:
+        result = a/b
+    except ZeroDivisionError:
+        result = "None"
+    finally:
+        print("Inside result: {}".format(result))
+    return result
