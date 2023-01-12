f5c897595d771dee687a835c7996cab153329c30
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-12 17:24:50 -0600

    Upload of task one on first project

--- 0x02-python-data_structures/0-print_list_integer.py
+++ 0x02-python-data_structures/0-print_list_integer.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+
+def print_list_integer(my_list=[]):
+    for x in my_list:
+        print(x)
+
+if __name__ == "__main__":
+    foo = [1,2,3,4]
+    print_list_integer(foo)
--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -2,4 +2,20 @@
 
 
 def roman_to_int(roman_string):
-    pass
+
+    digits = {
+        "i": 1,
+        "v": 5,
+        "x": 10,
+        "l": 50,
+        "c": 100,
+        "d": 1000
+    }
+    roman_string = roman_string.lower()
+
+    invalid_input = not (
+        all(x for x in list(digits.keys()) for x in roman_string) and
+        len(roman_string) > 0
+    )
+    if invalid_input:
+        return None
