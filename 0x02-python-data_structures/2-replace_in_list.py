22db33e44ffaef80dde58326542d9c086c0f4daf
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-12 17:42:31 -0600

    Inital attempt at third task

--- 0x02-python-data_structures/2-replace_in_list.py
+++ 0x02-python-data_structures/2-replace_in_list.py
@@ -2,4 +2,8 @@
 
 
 def replace_in_list(my_list, idx, element):
-    pass
+    if ((idx > (len(my_list) - 1)) or (idx < 0)):
+        return (my_list)
+    else:
+        my_list[idx] = element
+        return (my_list)
