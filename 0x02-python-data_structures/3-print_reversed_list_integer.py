5a5253c6a1bacd7efb4906346a4873d790d6545f
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-14 13:58:21 -0600

    Inital upload of forth task

--- 0x02-python-data_structures/3-print_reversed_list_integer.py
+++ 0x02-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,11 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    pass
+    my_list.reverse()
+
+    for x in my_list:
+        print("{:d}".format(x))
+
+if __name__ == "__main__":
+    foo = [1,2,3,4]
+    print_reversed_list_integer(foo)
