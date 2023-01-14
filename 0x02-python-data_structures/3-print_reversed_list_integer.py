1ff948ab5702b1cda7a87f5701de92197cd1c834
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-14 14:24:56 -0600

    PEP8 experiment

--- 0x02-python-data_structures/3-print_reversed_list_integer.py
+++ 0x02-python-data_structures/3-print_reversed_list_integer.py
@@ -6,7 +6,3 @@ def print_reversed_list_integer(my_list=[]):
 
     for x in my_list:
         print("{:d}".format(x))
-
-if __name__ == "__main__":
-    foo = [1,2,3,4]
-    print_reversed_list_integer(foo)
