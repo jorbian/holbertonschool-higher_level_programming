31434b1648c66efb292e2aed99348aaf5eea07e6
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-14 14:06:15 -0600

    Trying this again with fifth task

--- 0x02-python-data_structures/4-new_in_list.py
+++ 0x02-python-data_structures/4-new_in_list.py
@@ -2,7 +2,11 @@
 
 
 def new_in_list(my_list, idx, element):
-    new_list = my_list
+
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
+        
+    new_list = [x for x in my_list]
     new_list[idx] = element
 
     return new_list
