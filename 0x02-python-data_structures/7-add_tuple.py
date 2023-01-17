b7d86cfed7cb5d43599cab855cf8cc6f827bbde7
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-17 10:46:21 -0600

    asdf

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -12,7 +12,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     check_tuple(tuple_a)
     check_tuple(tuple_b)
 
-    return (
-        tuple_a[0] + tuple_b[0],
-        tuple_a[1] + tuple_b[1]
-    )
+    return ((
+        (tuple_a[0] + tuple_b[0]),
+        (tuple_a[1] + tuple_b[1])
+    ))
