11c09cdc6581b09962b2b6689d64c356b4ec0559
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-17 09:16:32 -0600

    First shot at tuple adding one

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -2,4 +2,16 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    pass
+    input_tuples = [tuple_a, tuple_b]
+
+    for tup in input_tuples:
+        if (len(tup) < 2):
+            tup = (tup[0], 0)
+        elif (len(tup) > 2):
+            tup = (tup[0], tup[1])
+
+    output = (
+        (input_tuples[0][0] + input_tuples[1][0]),
+        (input_tuples[0][1] + input_tuples[1][1])
+    )
+    return (output)
