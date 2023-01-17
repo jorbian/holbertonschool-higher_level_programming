bbc16c121b5b679892bbe677c6282b176ccc95b8
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-17 10:50:06 -0600

    asdf

--- 0x02-python-data_structures/7-add_tuple.py
+++ 0x02-python-data_structures/7-add_tuple.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 
 
+
+
 def add_tuple(tuple_a=(), tuple_b=()):
-    def check_tuple(tup):
-        if (len(tup) < 2):
-            tup = (tup[0], 0)
-        elif (len(tup) > 2):
-            tup = (tup[0], tup[1])
-        return (tup)
+    if (len(tuple_a) < 2):
+        if len(tuple_a) == 0:
+            tuple_a = (0, 0)
+        else:
+            tuple_a = (tuple_a[0], 0)
 
-    check_tuple(tuple_a)
-    check_tuple(tuple_b)
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = (0, 0)
+        else:
+            tuple_b = (tuple_b[0], 0)
 
-    return ((
-        (tuple_a[0] + tuple_b[0]),
-        (tuple_a[1] + tuple_b[1])
-    ))
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
