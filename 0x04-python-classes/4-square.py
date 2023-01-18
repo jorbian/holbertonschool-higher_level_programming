f95665425acbd81c761ccc89da75ebf196e51b96
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-17 23:05:45 -0600

    Last task of last project

--- 0x04-python-classes/4-square.py
+++ 0x04-python-classes/4-square.py
@@ -20,3 +20,24 @@ class Square:
     """
     def area(self):
         return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if (not isinstance(value, int)):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """Print the square with the # character."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if (self.__size == 0):
+            print("")
