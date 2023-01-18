fea0a57476933a8be0593a9c39b6dd5dd9c0e4f2
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-17 21:03:44 -0600

    Another attempt at tyask

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -2,6 +2,12 @@
 
 
 class Square:
-	def __init__(self, size):
-		self.__size = size
+	"""WAY TO REPRESENT A SQUARE"""
 
+	def __init__(self, size):
+		"""CLASS CONSTRUCTOR:
+		
+		ARGS:
+			size (int): size of the square
+		"""
+		self.__size = size
