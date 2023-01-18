78491b1747179dcc4785481780e2d7bfb4bc44cd
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-17 21:28:46 -0600

    Still trying to guess what the checker wants.

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,24 +1,8 @@
 #!/usr/bin/python3
+"""Class this is a simple Square"""
 
 
 class Square:
-    """Exceptions are documented in the same way as classes.
-
-    The __init__ method may be documented in either the class level
-    docstring, or as a docstring on the __init__ method itself.
-
-    Either form is acceptable, but the two should not be mixed. Choose one
-    convention to document the __init__ method and be consistent with it.
-
-    Note:
-        Do not include the `self` parameter in the ``Args`` section.
-
-    Args:
-        size (int): the size of the square
-
-    Attributes:
-        __size (int): size of square as private attribute
-    """
-
+    """Class that represents a square"""
     def __init__(self, size):
         self.__size = size
