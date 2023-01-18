597057eb110603416b5c9a1a577fcffa958ccff8
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-17 21:27:27 -0600

    Tweaking documentation.

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -2,12 +2,23 @@
 
 
 class Square:
-	"""WAY TO REPRESENT A SQUARE"""
+    """Exceptions are documented in the same way as classes.
 
-	def __init__(self, size):
-		"""CLASS CONSTRUCTOR:
-		
-		Args:
-			size (int): size of the square
-		"""
-		self.__size = size
+    The __init__ method may be documented in either the class level
+    docstring, or as a docstring on the __init__ method itself.
+
+    Either form is acceptable, but the two should not be mixed. Choose one
+    convention to document the __init__ method and be consistent with it.
+
+    Note:
+        Do not include the `self` parameter in the ``Args`` section.
+
+    Args:
+        size (int): the size of the square
+
+    Attributes:
+        __size (int): size of square as private attribute
+    """
+
+    def __init__(self, size):
+        self.__size = size
