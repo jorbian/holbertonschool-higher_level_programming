60c2ff48b3471cf121d2204ef8f6a6d4200205b3
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-17 21:46:44 -0600

    Fix PEP8

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -3,21 +3,19 @@
 """CLASS DOCUMENTATION GOES HERE"""
 
 class Square:
-	"""REPRESENTATION OF A SQUARE WITH PRIVATE SIZE
-	"""
+    """REPRESENTATION OF A SQUARE WITH PRIVATE SIZE
+    """
 
-	def __init__(self, size=0):
-		"""CLASS CONSTRUCTOR FOR SQARE"
-		"""
-		if type(size) != int:
-			raise TypeError("size must be an integer")
-		if (size < 0):
-			raise ValueError("size must be >= 0")
-		self.__size = size
+    def __init__(self, size=0):
+        """CLASS CONSTRUCTOR FOR SQARE"
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
-	"""RETURN THE AREA
-	"""
-	def area(self):
-		return (self.__size ** 2)
-
-
+    """RETURN THE AREA
+    """
+    def area(self):
+        return (self.__size ** 2)
