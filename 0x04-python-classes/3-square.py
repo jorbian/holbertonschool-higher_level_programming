4295b9ac0050deba12a0cda62ba03746ada88e5f
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-17 22:56:57 -0600

    Updated version of tasks

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -1,3 +1,35 @@
 #!/usr/bin/python3
 
+"""CLASS DOCUMENTATION GOES HERE"""
 
+
+class Square:
+    """REPRESENTATION OF A SQUARE WITH PRIVATE SIZE
+    """
+
+    def __init__(self, size=0):
+        """CLASS CONSTRUCTOR FOR SQARE"
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if (size < 0):
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    """RETURN THE AREA
+    """
+    def area(self):
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
