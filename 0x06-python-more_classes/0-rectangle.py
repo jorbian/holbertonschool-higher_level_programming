be28c30b522539e1ebae1a4b6bd86d1e5b7afb1a
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-22 21:32:39 -0600

    First attempt at first task

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    def width(self):
+        """Get the width of rectangle"""
+        return self.__width
+
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
