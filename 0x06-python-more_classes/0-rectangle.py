446e96e083144e12816266d281c6f62c50d5c936
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-24 01:38:27 -0600

    Corrected with the overloading thing I didn't understand...

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -15,10 +15,12 @@ class Rectangle:
         self.width = width
         self.height = height
 
+    @property
     def width(self):
         """Get the width of rectangle"""
         return self.__width
 
+    @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be integer")
@@ -26,10 +28,12 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
     def height(self):
         """Get the width of rectangle"""
         return self.__height
 
+    @height.setter
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be integer")
