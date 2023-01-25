179a6786f61c29e27a3e3b5386d2bd3c2fcc1eed
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-24 18:32:04 -0600

    Inital upload of third task

--- 0x06-python-more_classes/2-rectangle.py
+++ 0x06-python-more_classes/2-rectangle.py
@@ -43,3 +43,17 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        """Return printable representation of Rectangle.
+        Represents blocks of rectangle with '#'--
+        """
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+
+        rect = []
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
