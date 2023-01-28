9902f3505d85426b81109cf48bfe15c3659e46cd
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-27 19:13:13 -0600

    First attempt at second task

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 
+"""Add a custom sorting method to a new list class"""
+
+
+class MyList(list):
+    """EXACTLY WHAT IT SAYS ON THE TIN"""
+
+    def print_sorted(self):
+        print(sorted(self))
