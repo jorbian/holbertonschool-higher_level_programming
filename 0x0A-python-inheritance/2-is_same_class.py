00ad8219eb2b53195e4f03fd8c3e658981183923
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-27 19:22:10 -0600

    First attempt at third task

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 
+"""Wraper for test to see if object is of certain class"""
+
+def is_same_class(obj, a_class):
+    """Exactly what it says on the tin"""
+
+    if type(obj) == a_class:
+        return True
+    return False
