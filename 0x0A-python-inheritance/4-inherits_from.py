dc64c3f85c8124408d1b66e2092e60b9cc1918e4
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-27 20:00:14 -0600

    First attempt at fifth task.

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 
+"""Defines an inherited class-checking function."""
+
+
+def inherits_from(obj, a_class):
+    """Checks if an object is an inherited instance of a class."""
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
