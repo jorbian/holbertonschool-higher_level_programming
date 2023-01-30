0f715d75eda762a8f2b83f3ab6fb7fd4e6b5b277
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-29 19:38:59 -0600

    First attempt at sixth task.

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+"""Function that writes Object to a text file, using a JSON representation:"""
+import json
 
+
+def save_to_json_file(my_obj, filename):
+    """MORE DOCUMENATION WOULD GO HERe"""
+    with open(filename, 'w', encoding="utf-8") as file:
+        json.dump(my_obj, file)
