1a6a5e1ec21d25664cc263d1eb55824e5d5fa680
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-29 19:25:13 -0600

    Another tweak

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,4 +6,4 @@
 def write_file(filename="", text=""):
     """MORE DOCUMENATION WOULD GO HERe"""
     with open(filename, 'w', encoding="utf-8") as file:
-        file.write(text)
+        return file.write(text)
