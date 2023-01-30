b0ac0a61173bf84b6e85075934c6f07ee7c76086
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-29 19:03:34 -0600

    Another attempt to fix PEP8

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,9 +3,9 @@
 """Defines an inherited list class MyList."""
 
 
+
 class MyList(list):
     """Documentation would go here"""
-
     def print_sorted(self):
         _t = self[:]
         _t.sort()
