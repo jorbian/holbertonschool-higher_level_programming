b3e52dd2bd734d367afa855c8fc487c7ed5bcc54
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-29 19:14:59 -0600

    Attempt to fix PEP8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
+"""Define textfile-reading function."""
+
 
 def read_file(filename=""):
+    """MORE DOCUMENATION WOULD GO HERe"""
     if filename:
-        with open(filename) as file:
+        with open(filename, encoding="utf-8") as file:
             lines = file.readlines()
         print(lines)
