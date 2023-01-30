c0d554617b102226adf00ca928ff58fa6c93536d
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-29 19:27:57 -0600

    Attempt at third task

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,8 +3,7 @@
 """Define textfile-reading function."""
 
 
-def read_file(filename=""):
+def append_write(filename="", text=""):
     """MORE DOCUMENATION WOULD GO HERe"""
-    with open(filename, encoding="utf-8") as file:
-        lines = file.readlines()
-    print(lines)
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)
