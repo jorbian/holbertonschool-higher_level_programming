33ac815d92d94105312ea65df45110ce18adb68b
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-29 19:17:57 -0600

    Still trying to get those checks I'm missing for some reason.

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,4 +7,4 @@ def read_file(filename=""):
     """MORE DOCUMENATION WOULD GO HERe"""
     with open(filename, encoding="utf-8") as file:
         lines = file.readlines()
-    print(lines)
+    print(lines, end="")
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 
+"""Define textfile-reading function."""
+
+
+def read_file(filename=""):
+    """MORE DOCUMENATION WOULD GO HERe"""
+    with open(filename, encoding="utf-8") as file:
+        lines = file.readlines()
+    print(lines)
