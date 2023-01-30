7a4a292f835852f6fe6849d952b30aea5e1c09e5
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-29 19:35:19 -0600

    First attempt at fifth task

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+"""Function that returns an object represented by a JSON string:"""
+import json
 
+
+def from_json_string(my_str):
+    """MORE DOCUMENATION WOULD GO HERe"""
+    return json.loads(my_str)
