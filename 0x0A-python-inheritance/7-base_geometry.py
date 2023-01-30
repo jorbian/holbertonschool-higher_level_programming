baecb68434a50b937600b451cbec6411888a8f86
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-29 18:46:26 -0600

    Eight task

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -4,5 +4,12 @@
 
 
 class BaseGeometry:
-    """DOCUMENTATUON WOULD GO HERE"""
-    pass
+    """ALL THE GOOD STUFF WILL EVENTUALLY GO HERE"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if (not isinstance(value, int)):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
