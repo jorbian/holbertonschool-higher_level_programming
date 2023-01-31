5d472649b1cc1c26a1a8dcbbb43e56ecaae7a530
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-30 21:23:20 -0600

    Giving tenth task a shot.

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-"""Expansion of class Student defining a student """
+"""a class Student that defines a student"""
 
 
 class Student:
-    """The magic happens here"""
-    pass
+    """EXACTLY WHAT IT SAYS ON THE TIN"""
+
+    def __init__(self, first_name, last_name, age):
+        """INITALIZE """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Return the dictionary represntation of a simple data structure."""
+        return self.__dict__
