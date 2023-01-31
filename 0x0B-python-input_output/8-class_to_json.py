7f569c57bd321528775e547485e3eeabe5bd288d
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-30 21:16:48 -0600

    asdf

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
+"""function that returns the dictionary description of object for JSON"""
 
+
+def class_to_json(obj):
+    """Return the dictionary represntation of a simple data structure."""
+    return obj.__dict__
