b16f55315e14180bd04d8472a4767e3ee5021c85
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-30 19:46:44 -0600

    Eight task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,12 +7,7 @@ import json
 load = __import__('6-load_from_json_file').load_from_json_file
 save = __import__('5-save_to_json_file').save_to_json_file
 
+items = sys.argv[1:]
 
-def add_items():
-    items = [x for x in sys.argv[1:]]
-
-    with open("add_item.json", "w") as f:
-        json.dump(items, f)
-
-if __name__ == "__main__":
-    add_items()
+with open("add_item.json", "w", encoding="utf-8") as file:
+    json.dump(items, file)
