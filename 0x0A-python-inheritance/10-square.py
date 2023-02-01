4d4a5fb414d90439472a5e6ee28def18d04077e0
jorbian <60049930+jorbian@users.noreply.github.com>
2023-01-31 19:44:42 -0600

    Square attempt

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+Rectangle = __import__('9-rectangle.py').BaseGeometry
 
+
+class Square(Rectangle):
+    """Represent a rectangle using BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Intialize a new Rectangle."""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """CALC AREA OF RECT"""
+        return self.__width * self.__height
