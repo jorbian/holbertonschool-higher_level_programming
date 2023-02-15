78b94a036c4eafa7d44c3a9c9d9685c3d4476aff
jorbian <60049930+jorbian@users.noreply.github.com>
2023-02-14 22:04:20 -0600

    Last two tasks

--- 0x00-SQL_introduction/15-groups.sql
+++ 0x00-SQL_introduction/15-groups.sql
@@ -1,2 +1,3 @@
 -- Script that lists number of records with same score in table 'second_table'
 -- of the database
+SELECT score 'score', COUNT(score) 'number' FROM second_table GROUP BY score ORDER BY COUNT(score) DESC;
--- 0x00-SQL_introduction/16-no_link.sql
+++ 0x00-SQL_introduction/16-no_link.sql
@@ -1,1 +1,2 @@
 -- Script that lists all records of table 'second_table' of the database
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
