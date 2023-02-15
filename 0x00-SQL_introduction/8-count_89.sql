a2420d1e882127465ca43682dfbc1e4d08df42e9
jorbian <60049930+jorbian@users.noreply.github.com>
2023-02-14 21:42:50 -0600

    Inital upload of tenth task

--- 0x00-SQL_introduction/8-count_89.sql
+++ 0x00-SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
--- Displays number of records with id = 89 in able first_table of database
+-- Displays number of records with id = 89 in table first_table of database
 SELECT COUNT(*) FROM first_table WHERE id = 89;
