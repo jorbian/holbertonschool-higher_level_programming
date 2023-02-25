acb4fd47407632af853e76fe5d889de4b3098031
jorbian <5533@holbertonstudents.com>
2023-02-24 23:45:35 -0600

    Add exec perms and fixed a typo

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+""" LISTS STATES WITH NAME STARTING WITH 'N' FROM DATABASE 'hbtn_0e_0_usa.'
+USAGE: ./1-filter_states.py $USERNAME $PASSWORD $DB_NAME
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
