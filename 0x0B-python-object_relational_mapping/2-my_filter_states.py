f9a9b26ba56df19ac703be288663f805fc1916d4
jorbian <5533@holbertonstudents.com>
2023-02-26 18:01:04 -0600

    New attempt with fix to PEP8

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -3,22 +3,27 @@
 DISPLAY ALL VALUES IN 'states' TABLE OF DATABASE 'hbtn_0e_0_usa' WHOSE NAME
 MATCHES VALUE SUPPLIED AS ARGUMENT:
 
-USAGE: ./2-my_filter_states.py <mysql username> <mysql password> <database name> \
+USAGE: ./2-my_filter_states.py <username> <db_password> <db_name>
 <state name searched>
 """
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
-    c = db.cursor()
-    c.execute("SELECT * \
-               FROM `states` \
-               WHERE BINARY `name` = '{}'".format(sys.argv[4])
-    )
-    [print(state) for state in c.fetchall()]
+
+    if len(sys.argv) < 5:
+        print("INSUFFICENT NUMBER OF ARGUMENTS")
+    else:
+        db = MySQLdb.connect(
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+        c = db.cursor()
+        c.execute(
+            "SELECT * FROM `states` WHERE BINARY `name` = '{}'".format(
+                sys.argv[4]
+            )
+        )
+        [print(state) for state in c.fetchall()]
 
