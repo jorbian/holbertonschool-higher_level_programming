6c08e56f8e39cd7081dcb9a6e3a79a1c2616f428
jorbian <5533@holbertonstudents.com>
2023-02-26 22:40:37 -0600

    asdf

--- 0x0B-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0B-python-object_relational_mapping/4-cities_by_state.py
@@ -9,9 +9,11 @@ import MySQLdb
 if __name__ == "__main__":
 
     database = MySQLdb.connect(
+        host='localhost',
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3]
+        db=sys.argv[3],
+        port=3306
     )
     cursor = database.cursor()
     cursor.execute(
