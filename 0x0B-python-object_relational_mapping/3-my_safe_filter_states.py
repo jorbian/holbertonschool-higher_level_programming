20ea708992ce36f06eddc1dfd3b16b0466893d77
jorbian <5533@holbertonstudents.com>
2023-02-26 18:43:48 -0600

    another new attempt

--- 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,11 +9,15 @@ import MySQLdb
 if __name__ == "__main__":
 
     db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states`")
-
+    cursor.execute(
+        'SELECT * FROM states WHERE BINARY name = %(state)s ORDER BY id;',
+        {'state': sys.argv[4]}
+    )
     [print(state) for state in cursor.fetchall() if state[1] == sys.argv[4]]
