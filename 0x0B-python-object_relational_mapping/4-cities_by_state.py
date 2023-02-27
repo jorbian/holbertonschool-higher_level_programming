2db92defe9016fac230d543ad749d9f07f2053b2
jorbian <5533@holbertonstudents.com>
2023-02-26 22:00:32 -0600

    inital upload of fifth task

--- 0x0B-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0B-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,26 @@
+
 #!/usr/bin/python3
+"""
+LISTS CITIES OF DATABASE 'hbtn_0e_4_usa' ordered by city id.
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if len(sys.argv) < 5:
+        sys.exit("INSUFFICENT NUMBER OF ARGUMENTS")
+
+    database = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = database.cursor()
+    cursor.execute(
+            """SELECT C.id, C.name, S.name
+            FROM cities C
+            INNER JOIN states S
+            ON C.state_id = S.id
+            ORDER BY C.id"""
+    )
+    [print(city) for city in cursor.fetchall()]
