bc7910bdd200fbcc3f4d67aa4a232007c97b5555
jorbian <5533@holbertonstudents.com>
2023-02-28 00:19:02 -0600

    Inital upload of eleventh task

--- 0x0B-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+""" LISTS 'state' OBJECT WITH NAME PASSED AS ARG """
+
+import sys
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+if __name__ == "__main__":
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    try:
+        state = (
+            session
+            .query(State)
+            .order(State.id)
+            .filter(
+                State.name == sys.argv[4]
+            )
+            .one()
+        )
+        print(state.id)
+    except Exception:
+        print("Not found")
