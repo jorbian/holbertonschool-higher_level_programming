93cf94331ad1a722a5c9a58cc6cbe27893e53e46
jorbian <5533@holbertonstudents.com>
2023-02-27 19:47:15 -0600

    Preemtively add executable perms to all files

--- 0x0B-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0B-python-object_relational_mapping/4-cities_by_state.py
@@ -8,23 +8,18 @@ import MySQLdb
 
 if __name__ == "__main__":
 
-    db = MySQLdb.connect(
+    database = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3]
     )
-    cur = db.cursor()
+    cursor = database.cursor()
 
-    cur.execute('SELECT cities.id, cities.name, states.name \
-                FROM cities LEFT JOIN states \
-                  ON cities.state_id = states.id')
-
-    results = cur.fetchall()
-
-    cur.close()
-    db.close()
-
-    for row in results:
-        print(row)
+    cursor.execute(
+        'SELECT cities.id, cities.name, states.name \
+        FROM cities LEFT JOIN states \
+        ON cities.state_id = states.id'
+    )
+    [print(city) for city in cursor.fetchall()]
