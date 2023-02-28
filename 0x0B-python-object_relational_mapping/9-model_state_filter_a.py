565231f25004dfb0d3d01b4f84d10076ec410932
jorbian <5533@holbertonstudents.com>
2023-02-28 00:00:14 -0600

    Inital upload of tenth task

--- 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+""" SCRIPT TO LIST STATES IN TABLE CONTAINING 'a' """
+
+import sys
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+
+if __name__ == "__main__":
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in (
+        session.query(State)
+        .order_by(State.id)
+        .filter(
+            State.name
+            .contains('a'))
+    ):
+        print(f"{state.id}: {state.name}")
