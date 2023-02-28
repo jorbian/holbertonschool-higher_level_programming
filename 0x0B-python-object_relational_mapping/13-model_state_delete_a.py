de16c2be9dcea76ddf3d4ad742865ab6c9302443
jorbian <5533@holbertonstudents.com>
2023-02-28 01:02:44 -0600

    Inital upload of forteenth task

--- 0x0B-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0B-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""
+DELETES ALL 'States' with 'name' CONTAINING SPECIFIED LETTER FROM DATABASE
+'hbtn_0e_6_usa.'
+"""
+import sys
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+if __name__ == "__main__":
+
+    target_letter = "a"
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State):
+        if target_letter in state.name:
+            session.delete(state)
+
+    session.commit()
