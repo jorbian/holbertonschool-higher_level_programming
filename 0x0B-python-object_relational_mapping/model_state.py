cd07e0f63fb6a0279858676fc0ace69a3854947a
jorbian <5533@holbertonstudents.com>
2023-02-27 22:41:24 -0600

    inital upload of model state

--- 0x0B-python-object_relational_mapping/model_state.py
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" DEFINES A MODEL FOR STATES IN DATABASE """
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """REPRESENTS A US STATE FOR DATABASE"""
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
