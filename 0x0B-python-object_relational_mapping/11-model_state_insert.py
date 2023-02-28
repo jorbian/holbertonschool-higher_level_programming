d74ce5b5a088085ef4b8612e0dd9c82152c119a0
jorbian <5533@holbertonstudents.com>
2023-02-28 00:40:42 -0600

    Inital upload of twelth task

--- 0x0B-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0B-python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+""" ADDS 'state' OBJECT 'Lousiana' to DATABASE 'hbtn_0e_6_usa.' """
+
+import sys
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+if __name__ == "__main__":
+
+    new_state_name = "Louisiana"
+
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name=new_state_name)
+    
+    session.add(new_state)
+    session.commit()
+
+    print(new_state.id)
