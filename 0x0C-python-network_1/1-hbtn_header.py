7b425c11e6d4fbbf8f5b71a0c8c94ca872a0ed38
Jonathan Boller <60049930+jorbian@users.noreply.github.com>
2023-03-10 16:01:03 -0600

    Initial upload of second task

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
+"""
+AN EXPRIMENT WITH A FUNCTIONAL APPROACH TO TASK.
+I WILL EVENTUALLY CLEAN THIS REPO UP TO TAKE BETTER ADVANTAGE OF IT/
+"""
+import sys
+import urllib.request
 
-print("Hello world!")
+
+def retrieve_header(url, header, do_action=(lambda x: x)):
+    """IT NOT ONLY DOESNT CARE WHAT HEADER YOU WANT BUT WANT TO DO WITH IT"""
+    with urllib.request.urlopen(url) as page:
+        header = do_action(
+            (page.headers).get(header)
+        )
+    return header
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    retrieve_header(url, "X-Request-Id", print)
