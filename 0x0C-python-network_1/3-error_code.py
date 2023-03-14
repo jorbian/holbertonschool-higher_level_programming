80074e09e1759564b644f6e81efeaabf3073f4c3
jorbian <60049930+jorbian@users.noreply.github.com>
2023-03-14 10:47:14 -0500

    Inital upload of forth task

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
+"""Sends request to a given URL and displays the response body.
+"""
+import sys
+import urllib.error
+import urllib.request
 
-print("Hello world!")
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
