76616ab11c3763a901d993b0a1f16407ba6d802f
jorbian <60049930+jorbian@users.noreply.github.com>
2023-03-14 10:55:11 -0500

    Inital upload of fifth task

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status."""
+import requests
 
-print("Hello world!")
+
+if __name__ == "__main__":
+    request = requests.get("https://intranet.hbtn.io/status")
+    print(
+        "Body response:\n\t- type: {}\n\t- content: {}".format(
+            type(request.text),
+            request.text
+        )
+    )
