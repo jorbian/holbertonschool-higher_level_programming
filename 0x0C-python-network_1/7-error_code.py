0d46d73fedb9f9c52f7dfd389e0df10fecded122
jorbian <60049930+jorbian@users.noreply.github.com>
2023-03-14 13:38:50 -0500

    Update 7-error_code.py
    Fixed PEP 8

--- 0x0C-python-network_1/7-error_code.py
+++ 0x0C-python-network_1/7-error_code.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
 
     r = requests.get(url)
-    
+
     message_strings = [
         ((r.status_code >= 400), ("Error code: {}".format(r.status_code))),
         (True, r.text)
@@ -18,5 +18,5 @@ if __name__ == "__main__":
         if test[0]:
             display_s = test[1]
             break
-        
+
     print(display_s)
