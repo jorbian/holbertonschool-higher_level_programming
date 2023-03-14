33f36eef1c543f4d2692c2e9c0cf590576045ebd
jorbian <60049930+jorbian@users.noreply.github.com>
2023-03-14 13:30:12 -0500

    Inital upload of the eighth task

--- 0x0C-python-network_1/7-error_code.py
+++ 0x0C-python-network_1/7-error_code.py
@@ -1,3 +1,22 @@
 #!/usr/bin/python3
+"""SENDS REQUEST TO A GIVEN URL AND DISPLAYS THE RESPONSE BODY.
+"""
+import sys
+import requests
 
-print("Hello world!")
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    
+    message_strings = [
+        ((r.status_code >= 400), ("Error code: {}".format(r.status_code))),
+        (True, r.text)
+    ]
+    for test in message_strings:
+        if test[0]:
+            display_s = test[1]
+            break
+        
+    print(display_s)
