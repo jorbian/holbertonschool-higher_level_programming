020614fc3eb251fc150d5dffdb13e202695fe709
jorbian <60049930+jorbian@users.noreply.github.com>
2023-03-14 11:21:19 -0500

    Inital upload of seventh task

--- 0x0C-python-network_1/6-post_email.py
+++ 0x0C-python-network_1/6-post_email.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""SENDS 'POST' REQUEST TO GIVEN URL WITH EMAIL --
+-- DISPLAYS THE BODY OF THE RESPNSE
+"""
+import sys
+import requests
 
-print("Hello world!")
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {
+        "email": sys.argv[2]
+    }
+    request = requests.post(url, data=value)
+    print(request.text)
