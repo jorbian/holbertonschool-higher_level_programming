3f022bb82d429b9a5cacd014b35f8adfd50a99b1
jorbian <5533@holbertonstudents.com>
2023-04-08 00:11:01 -0500

    Twelth task

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+
+const numbers = (
+  (process.argv).slice(2)
+  .map(Number)
+);
+
+if (numbers.length < 2) {
+  console.log(0);
+} else {
+  console.log(
+    numbers.sort()[numbers.length - 2]
+  );
+}
