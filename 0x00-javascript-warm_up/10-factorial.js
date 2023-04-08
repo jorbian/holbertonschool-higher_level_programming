edf67214866d22aef9ec5f1aea0f08208401506d
jorbian <5533@holbertonstudents.com>
2023-04-07 23:51:18 -0500

    Inital attempt at eleventh task

--- 0x00-javascript-warm_up/10-factorial.js
+++ 0x00-javascript-warm_up/10-factorial.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+const inputNum = Math.floor(
+  Number(
+    process.argv[2]
+  )
+);
+
+function factorial(x) {
+  if (x < 1) {
+    return 1;
+  } else {
+    return x * factorial(x - 1);
+  }
+}
+
+console.log(factorial(inputNum));
