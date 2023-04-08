dbb85a5c21b6b66f3d3c0665598b4cf8bfc5c305
jorbian <5533@holbertonstudents.com>
2023-04-08 00:19:40 -0500

    twelth

--- 0x00-javascript-warm_up/11-second_biggest.js
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -9,7 +9,7 @@ if (numbers.length < 2) {
   console.log(0);
 } else {
   numbers = numbers.map(num => parseInt(num));
-  const max = Math.max(...nums);
-  nums = nums.filter(num => num !== max);
-  console.log(Math.max(...nums));
+  const max = Math.max(...numbers);
+  numbers = nums.filter(num => num !== max);
+  console.log(Math.max(...numbers));
 }
