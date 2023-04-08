e91236bb28d67aa02d652613ee5f563479c6a5ae
jorbian <5533@holbertonstudents.com>
2023-04-07 23:13:51 -0500

    Inital attempt at eight task

--- 0x00-javascript-warm_up/7-multi_c.js
+++ 0x00-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+const message = 'C is fun';
+const numTimes = Math.floor(
+  Number(
+    process.argv[2]
+  )
+);
+
+if (isNaN(numTimes)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < message; i++) {
+    console.log(message);
+  }
+}
