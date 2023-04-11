45adb470143327efdd6a5a0541410ffbcfe806fd
jorbian <60049930+jorbian@users.noreply.github.com>
2023-04-11 11:39:24 -0500

    Still looking to fix style problem blind.

--- 0x01-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,8 +2,11 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) [this.width, this.height] = [w, h];
-	}
-}
+    if (w > 0 && h > 0) {
+			[this.width, this.height] = [w, h];
+		}
+  }
+};
 
 module.exports = Rectangle;
+  
