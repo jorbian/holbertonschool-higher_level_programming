540383e89356f4fb480a9729d42fb49e1ad806fa
jorbian <5533@holbertonstudents.com>
2023-04-11 22:12:27 -0500

    Testing script

---
+++ 0x01-javascript_objects_scopes_closures/fix_node.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+from os.path import abspath
+from pathlib import Path
+
+THIS_FOLDER = Path(abspath(__file__)).parent
+
+def fix_node_files(folder_obj):
+    
+    js_files = folder_obj.glob('*.js')
+    for file in js_files:
+        subprocess.run(["semistandard", "fix", file.name])
+
+if __name__ == "__main__":
+    
+    fix_node_files(THIS_FOLDER)
