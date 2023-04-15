369a316f22f9c33eb9c7896accfde924b46cba1a
jorbian <5533@holbertonstudents.com>
2023-04-15 12:43:07 -0500

    Messing around with style.

--- 0x02-javascript-web_scraping/0-readme.js
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -2,10 +2,14 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
+fs.readFile(
+  process.argv[2],
+  'utf8',
+  function (err, data) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(data);
+    }
   }
-});
+);
