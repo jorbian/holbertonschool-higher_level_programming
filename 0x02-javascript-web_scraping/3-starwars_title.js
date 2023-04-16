dc26b33f179cfdd2b77805a65df95b24be7ccdff
jorbian <5533@holbertonstudents.com>
2023-04-15 21:41:01 -0500

    Forth task

--- 0x02-javascript-web_scraping/3-starwars_title.js
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
+
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
-});
+
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+
+request(
+  url,
+  (error, body) => console.log(error || JSON.parse(body).title)
+);
