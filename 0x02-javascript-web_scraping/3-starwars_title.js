77d3b091c520f6067b528f451bcf430a5dfb829b
jorbian <5533@holbertonstudents.com>
2023-04-15 21:28:01 -0500

    Inital upload of forth task

--- 0x02-javascript-web_scraping/3-starwars_title.js
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+
+request(
+  url,
+  (error, body) => console.log(error || JSON.parse(body).title)
+);
