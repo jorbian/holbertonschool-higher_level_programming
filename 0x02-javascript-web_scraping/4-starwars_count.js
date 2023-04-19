7d2315dcdc65c107e23477058cc6c97d58417f04
jorbian <5533@holbertonstudents.com>
2023-04-18 19:41:14 -0500

    asdf

--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -2,15 +2,20 @@
 
 const request = require('request');
 
-request(
-    process.argv[2],
-    function (error, body) {
-        if (!error) {
-            const results = JSON.parse(body).results;
-            console.log(results.reduce((count, movie) => {
-            return movie.characters.find((character) => character.endsWith('/18/'))
-            ? count + 1
-        : count;
-    }, 0));
+request(process.argv[2], function (err, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const data = JSON.parse(body).results;
+    let count = 0;
+    for (const title in data) {
+      const charList = data[title].characters;
+      for (const person in charList) {
+        if (charList[person].includes('/18/')) {
+          count++;
+        }
+      }
+    }
+    console.log(count);
   }
 });
--- 0x02-javascript-web_scraping/5-request_store.js
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+request(process.argv[2], function (err, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.appendFile(process.argv[3], body, function (writeError) {
+      if (writeError) {
+        console.log(writeError);
+      }
+    });
+  }
+});
